c49a4b614238fb0b789aed6f80741850baf1dc41
gtgeshev <g.t.geshev@gmail.com>
2017-03-12 14:17:45 -0500

    Add files via upload

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import AccountsUI from '../AccountsUI.jsx';
 
 export const MainLayout = ({content}) => (
+	<div>
+	
+	<div className="jumbotron">
+		<h1 className="text-center">Kirkfield Web Portal</h1>
+	</div>
+		
 	<div className="container">
 	<header>
-		<h2>Kirkfield Web Portal</h2>
-		<nav className="navbar navbar-default">
+		<div className="row">
+		
+		<nav className="navbar navbar-default text-center">
 			<div>
 				<div className="navbar-header">
-				<a className="navbar-brand" href="#">Kirkfield</a>
+				<a className="navbar-brand" href="/">Kirkfield</a>
 				</div>
 				<ul className="nav navbar-nav">
 					<li className="dropdown">
@@ -36,15 +43,19 @@ export const MainLayout = ({content}) => (
 					</li>
 					<li><a href="/employees">Employees</a></li>
 					<li><a href="/reporting">Reporting</a></li>
+					<li><a href="/admin">Admin</a></li>
 					<li><AccountsUI /></li>
 				</ul>
 			</div>
 		</nav>
+		</div>
 	</header>
 	<main>
 		<div>
 			{content}
 		</div>
 	</main>
+	</div>
+	
 	</div>
 )

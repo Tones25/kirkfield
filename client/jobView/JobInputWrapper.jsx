e47d6a639884f8277582a3294aadf08fd75502be
cgaudry <gaudry-c4@webmail.uwinnipeg.ca>
2017-03-12 23:38:29 -0500

    Add files via upload

--- client/jobView/JobInputWrapper.jsx
+++ client/jobView/JobInputWrapper.jsx
@@ -6,6 +6,7 @@ import JobForm from './JobForm.jsx';
 import JobSingle from './JobSingle.jsx';
 
 import {Vehicles} from './../vehicleView/VehicleInputWrapper.jsx';
+import DataTable from './../DataTable.jsx';
 
 export const Jobs = new Mongo.Collection("jobs");
 
@@ -17,13 +18,22 @@ export default class JobInputWrapper extends TrackerReact(React.Component) {
 			subscription: {
 				jobs: Meteor.subscribe("allJobs"),
 				vehicles: Meteor.subscribe("allVehicles")
-			}
-		}
+			},
+			search: ""
+		};
+		this.handleSearchChange = this.handleSearchChange.bind(this);
 	}
 
 	componentWillUnmount() {
 		this.state.subscription.jobs.stop();
 		this.state.subscription.vehicles.stop();
+	}
+
+	handleSearchChange() {
+		let search = document.getElementById("search").value;
+		this.setState({
+			search: search,
+		});
 	}
 
 	jobItems() {
@@ -36,6 +46,7 @@ export default class JobInputWrapper extends TrackerReact(React.Component) {
 
 	render() {
 		
+		let tableRowHeight = 50;
 		return(
 		<div className="row">
 			<div className="panel panel-primary">
@@ -48,14 +59,27 @@ export default class JobInputWrapper extends TrackerReact(React.Component) {
 			</div>
 			<div className="panel panel-primary">
 				<div className="panel-heading">
-					<h1>Recently Added Jobs</h1>
+					<h1>Job Listing</h1>
 				</div>
-				<div className="panel-body">
-				<ul className="resolutions">
-					{this.jobItems().map( (jobItems) => {
-						return <JobSingle key={jobItems._id} jobItem={jobItems} />
-					})}
-				</ul>
+				<div className="panel-body">		
+					<input 
+						type="text" 
+						className="form-control"
+						id="search"
+						ref="search"
+						placeholder="Search"
+					onChange={this.handleSearchChange.bind(this)}
+					/>
+					<DataTable 
+						rowHeight={tableRowHeight}
+						columns={['invoice', 'customer', 'installCost', 'installEmployee']}
+						columnNames={['Invoice#', 'Customer ID#', 'Charge ($)', 'Employee ID#']}
+						deleteButtons={true}
+						deleteFunction={'deleteJobItem'}
+						editButtons={true}
+						editFunction={ function(route) {FlowRouter.go("/job/" + route._id);} }
+						data={this.jobItems()}
+					/>
 				</div>
 			</div>
 		</div>

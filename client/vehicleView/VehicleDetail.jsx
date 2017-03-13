265fb9b2e6487f7636b03baeb1fb3cf37594d7ca
cgaudry <gaudry-c4@webmail.uwinnipeg.ca>
2017-03-12 23:43:52 -0500

    Vehicle revamp

--- client/vehicleView/VehicleDetail.jsx
+++ client/vehicleView/VehicleDetail.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
-import {Vehicles} from './../vehicleView/VehicleInputWrapper.jsx';
+import {Vehicles} from './VehicleInputWrapper.jsx';
 
 export default class VehicleDetail extends TrackerReact(Component) {
 
@@ -23,6 +23,63 @@ export default class VehicleDetail extends TrackerReact(Component) {
 		return Vehicles.findOne(this.props.id);
 	}
 
+	date(oil) {
+		let vehicle = this.vehicle();
+		//console.log(customer);
+		if (oil == 'o')
+		newDate = new Date(parseInt(vehicle.lastOil.getFullYear()), parseInt(vehicle.lastOil.getMonth()), parseInt(vehicle.lastOil.getDate()));
+		if (oil == 'n')
+		newDate = new Date(parseInt(vehicle.nextOil.getFullYear()), parseInt(vehicle.nextOil.getMonth()), parseInt(vehicle.nextOil.getDate()));
+		return newDate.toISOString().substr(0,10);
+	}
+
+	editVehicle(event) {
+		event.preventDefault();
+		let vehicleMake = this.refs.vehicleMake.value.trim();
+		let vehicleModel = this.refs.vehicleModel.value.trim();
+		let vehicleModelYear = this.refs.vehicleModelYear.value.trim();
+		let licensePlate = this.refs.licensePlate.value.trim();
+		let color = this.refs.vehicleColor.value.trim();
+		let initialMileage = this.refs.initialMileage.value.trim();
+		let repairHist = this.refs.repairHist.value.trim();
+		let description = this.refs.description.value.trim();
+		let lastOil = this.refs.lastOil.value.trim();
+		let nextOil = this.refs.nextOil.value.trim();
+		let validInput = true;
+		
+		//add further input validation rules here
+		if (vehicleModelYear < 0) {
+			Bert.alert('year can\'t be negative', 'danger', 'fixed-top', 'fa-frown-o');
+			validInput = false;
+		}
+		if (validInput) {
+			Meteor.call('editVehicle', this.vehicle(), vehicleMake,
+				vehicleModel, vehicleModelYear, licensePlate, color, initialMileage, 
+				repairHist, description, lastOil, nextOil, (error, data) => {
+			if(error) {
+				Bert.alert(error.error, 'danger', 'fixed-top', 'fa-frown-o');
+			} else {
+				Bert.alert('Successfully updated Item: ' + vehicleModelYear + " " + vehicleMake + " " + vehicleModel, 'success', 'fixed-top', 'fa-smile-o');
+			this.refs.vehicleMake.value = "";
+			this.refs.vehicleModel.value = "";
+			this.refs.vehicleModelYear.value = "";
+			this.refs.licensePlate.value = "";
+			this.refs.color.value = "";
+			this.refs.initialMileage.value = 50000;
+			this.refs.licensePlate.value = "";
+			this.refs.description.value = "";
+			this.refs.repairHist.value = "";
+			this.refs.lastOil.value = "";
+			this.refs.nextOil.value = "";
+			}
+		});
+		}
+	}
+
+	back() {
+		FlowRouter.go("/vehicleInput");
+	}
+
 	render() {
 		let vehicle = this.vehicle();
 
@@ -30,20 +87,138 @@ export default class VehicleDetail extends TrackerReact(Component) {
 			return(<div>Loading...</div>)
 		}
 		return(
+			<div className="row">				
+				<button className="btn btn-primary" onClick={this.back.bind(this)}>
+						Back to Vehicles<span className="glyphicon glyphicon-return"></span>
+				</button>
 			<div className="panel panel-primary">
 				<div className="panel-heading">
-					<h1>Vehicle Id: {vehicle.vehicleId}</h1>
+					<h1>Vehicle #{vehicle.vehicleId}</h1>
 				</div>
 				<div className="panel-body">
-					<ul className="list-group">
-						<li className="list-group-item">
-							Vehicle Name: {vehicle.vehicleName}
-						</li>
-						<li className="list-group-item">
-							Vehicle Make: {vehicle.vehicleMake}
-						</li>
-					</ul>
+					<form className="form-horizontal" onSubmit={this.editVehicle.bind(this)}>
+					<div className="form-group">
+					
+					<label className="control-label col-sm-2" htmlFor="vehicleMake">Make:</label>
+					<div className="col-sm-2">
+					<input 
+						className="form-control"
+						id="vehicleMake"
+						type="text" 
+						ref="vehicleMake"
+						defaultValue={vehicle.vehicleMake}
+					/>
+					</div>
+					
+					<label className="control-label col-sm-2" htmlFor="vehicleModel">Model:</label>
+					<div className="col-sm-2">
+					<input 
+						className="form-control"
+						id="vehicleModel"
+						type="text" 
+						ref="vehicleModel"
+						defaultValue={vehicle.vehicleModel}
+					/>
+					</div>
+					
+					<label className="control-label col-sm-2" htmlFor="vehicleModelYear">Model Year:</label>
+					<div className="col-sm-2">
+					<input 
+						className="form-control"
+						id="vehicleModelYear"
+						type="number" 
+						ref="vehicleModelYear"
+						placeholder="Model Year"
+						defaultValue={vehicle.vehicleModelYear}
+					/>
+					</div>
+					
+					</div>
+					
+					<div className="form-group">
+					
+					<label className="control-label col-sm-2" htmlFor="licensePlate">License Plate:</label>
+					<div className="col-sm-2">
+					<input 
+						className="form-control"
+						id="licensePlate"
+						type="text" 
+						ref="licensePlate"
+						defaultValue={vehicle.licensePlate}
+					/>
+					</div>
+					
+					<label className="control-label col-sm-2" htmlFor="vehicleColor">Color:</label>
+					<div className="col-sm-2">
+					<input
+						className="form-control"
+						id="vehicleColor"
+						type="text"
+						ref="vehicleColor"
+						defaultValue={vehicle.color}
+					/>
+					</div>
+					
+					<label className="control-label col-sm-2" htmlFor="initialMileage">Mileage:</label>
+					<div className="col-sm-2">
+					<input
+						className="form-control"
+						id="initialMileage"
+						type="number"
+						ref="initialMileage"
+						defaultValue={vehicle.initialMileage}
+					/>
+					</div>
+					</div>
+					<div className="form-group">
+					<label className="control-label col-sm-2" htmlFor="description">Description:</label>
+					<div className="col-sm-5">
+					<input 
+						className="form-control"
+						id="description"
+						type="text" 
+						ref="description"
+						defaultValue={vehicle.description}
+					/>
+					</div>
+					<label className="control-label col-sm-2" htmlFor="lastOil">Last Oil Change:</label>
+					<div className="col-sm-3">
+					<input 
+						type="date" 
+						className="form-control"
+						id="lastOil"
+						ref="lastOil"
+						defaultValue={this.date('o')}
+					/>
+					</div>
+					</div>
+					<div className="form-group">
+					<label className="control-label col-sm-2" htmlFor="repairHist">Repair History:</label>
+					<div className="col-sm-5">
+					<textarea 
+						className="form-control"
+						id="repairHist"
+						cols="40" rows="5" 
+						ref="repairHist"
+						defaultValue={vehicle.repairHist}
+					/>
+					
+					</div>
+					<label className="control-label col-sm-2" htmlFor="nextOil">Next Oil Change:</label>
+					<div className="col-sm-3">
+					<input 
+						type="date" 
+						className="form-control"
+						id="nextOil"
+						ref="nextOil"
+						defaultValue={this.date('n')}
+					/>
+					</div>
+					</div>
+					<input type="submit" className="btn btn-primary pull-right"/>
+				</form>
 				</div>
+			</div>
 			</div>
 			)
 	}

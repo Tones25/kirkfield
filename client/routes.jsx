2a08ce4c8dda80b155cc042060a886c7d802335d
cgaudry <gaudry-c4@webmail.uwinnipeg.ca>
2017-03-15 20:06:23 -0500

    updated routes for employees

--- client/routes.jsx
+++ client/routes.jsx
@@ -21,10 +21,10 @@ import VehicleDetail from './vehicleView/VehicleDetail.jsx';
 import ReportWrapper from './reportView/ReportWrapper.jsx';
 
 import EmployeeInputWrapper from './employeeView/EmployeeInputWrapper.jsx';
+import EmployeeDetailWrapper from './employeeView/EmployeeDetailWrapper.jsx';
 
 
 import HomePage from './HomePage.jsx';
-
 
 FlowRouter.route('/', {
 	action() {
@@ -130,6 +130,15 @@ FlowRouter.route('/employees', {
 	action() {
 		mount(MainLayout, {
 			content: (<EmployeeInputWrapper />),
+		})
+	}
+})
+
+FlowRouter.route('/employee/:id', {
+	action(params) {
+		mount(MainLayout, {
+			//this passes params.id as a prop into RosultionDetail instance
+			content: (<EmployeeDetailWrapper id={params.id} />),
 		})
 	}
 })

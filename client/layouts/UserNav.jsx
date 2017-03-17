07bc0aa0c99561e05e3ffb0d1dd62a09465780d4
cgaudry <gaudry-c4@webmail.uwinnipeg.ca>
2017-03-17 15:11:29 -0500

    Navbar links update

--- client/layouts/UserNav.jsx
+++ client/layouts/UserNav.jsx
@@ -11,20 +11,8 @@ export default class AdminNav extends Component {
 		return (
 				<div>
 				<ul className="nav navbar-nav">
-					<li className="dropdown">
-						<a className="dropdown-toggle" data-toggle="dropdown" href="#">Inventory
-						<span className="caret"></span></a>
-						<ul className="dropdown-menu">
-							<li><a href="/inventorySearch">Inventory Search</a></li>
-						</ul>
-					</li>
-					<li className="dropdown">
-						<a className="dropdown-toggle" data-toggle="dropdown" href="#">Jobs
-						<span className="caret"></span></a>
-						<ul className="dropdown-menu">
-							<li><a href="/jobInput">Job Input</a></li>
-						</ul>
-					</li>
+					<li><a href="/inventoryInput">Inventory</a></li>
+					<li><a href="/jobInput">Jobs</a></li>
 					<li><a href="/customers">Customers</a></li>
 					<li><AccountsUI /></li>
 				</ul>

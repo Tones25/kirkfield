64117667585111d89eff30170d9198c16b3a2c27
trevorha <harrisatrevor@gmail.com>
2017-03-18 21:34:50 -0500

    homepage transitions

--- client/HomePage.jsx
+++ client/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 export default class HomePage extends Component {
 
 	setVar() {
@@ -28,8 +28,14 @@ export default class HomePage extends Component {
 
 	render() {
 		return(
+		
 		<div className="row">
-
+		<ReactCSSTransitionGroup
+					transitionName="inventoryButton"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}
+					transitionAppear={true}
+					transitionAppearTimeout={500}>
 			<div className="col-sm-6">
 				<button onClick={this.inv.bind(this)}
 						className="btn btn-primary btn-lg btn-block homePageButtons"
@@ -37,6 +43,14 @@ export default class HomePage extends Component {
 				<span className="glyphicon glyphicon-shopping-cart"></span> Inventory
 				</button>
 			</div>
+		</ReactCSSTransitionGroup>
+		
+		<ReactCSSTransitionGroup
+					transitionName="jobsButton"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}
+					transitionAppear={true}
+					transitionAppearTimeout={750}>
 			<div className="col-sm-6">
 				<button onClick={this.job.bind(this)}
 						className="btn btn-primary btn-lg btn-block homePageButtons"
@@ -44,7 +58,14 @@ export default class HomePage extends Component {
 				<span className="glyphicon glyphicon-briefcase"></span> Jobs
 				</button>
 			</div>
-			
+		</ReactCSSTransitionGroup>
+		
+		<ReactCSSTransitionGroup
+					transitionName="vehiclesButton"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}
+					transitionAppear={true}
+					transitionAppearTimeout={1000}>
 			<div className="col-sm-6">
 				<button onClick={this.veh.bind(this)}
 						className="btn btn-primary btn-lg btn-block homePageButtons"
@@ -52,6 +73,13 @@ export default class HomePage extends Component {
 				<span className="glyphicon glyphicon-road"></span> Vehicles
 				</button>
 			</div>
+		</ReactCSSTransitionGroup>
+		<ReactCSSTransitionGroup
+					transitionName="employeesButton"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}
+					transitionAppear={true}
+					transitionAppearTimeout={1250}>
 			<div className="col-sm-6">
 				<button onClick={this.emp.bind(this)}
 						className="btn btn-primary btn-lg btn-block homePageButtons"
@@ -59,7 +87,13 @@ export default class HomePage extends Component {
 				<span className="glyphicon glyphicon-user"></span> Employees
 				</button>
 			</div>
-			
+		</ReactCSSTransitionGroup>	
+		<ReactCSSTransitionGroup
+					transitionName="reportsButton"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={500}
+					transitionAppear={true}
+					transitionAppearTimeout={1500}>	
 			<div className="col-sm-12">
 				<button onClick={this.rep.bind(this)}
 						className="btn btn-primary btn-lg btn-block homePageButtons"
@@ -67,7 +101,7 @@ export default class HomePage extends Component {
 				<span className="glyphicon glyphicon-th-list"></span> Reports 
 				</button>
 			</div>
-
+		</ReactCSSTransitionGroup>
 		</div>
 		)
 	}

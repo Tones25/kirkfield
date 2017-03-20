21c958e0c5ea830123062f9953f068292ebb1f5b
trevorha <harrisatrevor@gmail.com>
2017-03-20 15:11:46 -0500

    quick navbar fix

--- client/layouts/AppHeader.jsx
+++ client/layouts/AppHeader.jsx
@@ -23,8 +23,8 @@ export default class AppHeader extends TrackerReact(React.Component) {
 	navType() {
 		let userRole = '';
 		if(Meteor.user()) {
-			userRole = Meteor.user().roles[0];
-			
+			if (Meteor.user().roles)
+				userRole = Meteor.user().roles[0];
 		}
 
 		if(userRole == 'admin')
